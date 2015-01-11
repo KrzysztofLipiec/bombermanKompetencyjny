73f177f844753707a669854cc60f48ad9cf030b9
Daniel Stanczak <deny1230@hotmail.com>
2015-01-11 20:36:57 +0100

    as

--- main.py
+++ main.py
@@ -2,60 +2,49 @@
 # coding=utf-8
 import datetime
 import json
-import new
-import os
 import time
 import uuid
-import webbrowser
-from google.appengine.ext.webapp import template
-import webapp2
+import os
 
-from websocket import create_connection
+import google.appengine._internal.django.utils
+from google.appengine.tools.devappserver2 import channel
+import webapp2
 from google.appengine.datastore import entity_pb
 from google.appengine.ext import db
-from google.appengine.ext import webapp
 from google.appengine.api import users
 from google.appengine.api import memcache
-
-import os
-import logging
-import wsgiref.handlers
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from common.handlers import base #PROBLEM Z INSTALACJĄ BIBLIOTEKI
 
-# def switch_accounts(request):
-# """ View function for allowing a user to switch Google accounts.
-# Requires 'destination_url' in the query string for the URL
-# to redirect the user to after they have switched accounts.
-# """
-# current_user_id = get_current_user().user_id()
-# previous_user_id = request.session.get('original_user_id')
-#     if previous_user_id and previous_user_id != current_user_id:
-#         #They have successfully switched accounts
-#         del request.session['original_user_id']
-#         final_destination = request.GET['destination_url']
-#         return redirect(final_destination)
-#     if previous_user_id and previous_user_id == current_user_id:
-#         #They’ve been to the login view and returned as the same
-#         #user, so they’re not using the multiple accounts feature.
-#         #To switch accounts they must log out, then back in again.
-#         login_url = create_login_url(dest_url=request.get_full_path())
-#         return redirect(create_logout_url(dest_url=login_url)))
-#         #else, we haven’t tried the multiple accounts test yet..
-#         request.session['original_user_id'] = current_user_id
-#         login_url = create_login_url(dest_url=request.get_full_path())
-#     return redirect(login_url)
+
+
+
+def GetLogoutUrl():
+    """Returns a logout url."""
+    return users.create_logout_url('/')
 
 
 class StronaGlowna(webapp.RequestHandler):
-    def get(self):
-        ws = create_connection("ws://localhost:8080/websocket")
-        print "Sending 'Hello, World'..."
-        ws.send("Hello, World")
-        print "Sent"
-        print "Reeiving..."
-        result =  ws.recv()
-        print "Received '%s'" % result
-        ws.close()
+    def get(self, q):
+        if q is None:
+            q = 'index.html'
+
+        path = os.path.join(os.path.dirname(__file__), q)
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(template.render(path, {}))
+
+        # def get(self):
+        # self.response.headers['cos'] = 'text/plain'
+
+        # ws = create_connection("ws://localhost:8080/websocket")
+        # print "Sending 'Hello, World'..."
+        # ws.send("Hello, World")
+        #print "Sent"
+        #print "Reeiving..."
+        #result =  ws.recv()
+        #print "Received '%s'" % result
+        #ws.close()
 
         # temp = os.path.join(
         #     os.path.dirname(__file__),
@@ -65,12 +54,50 @@ class StronaGlowna(webapp.RequestHandler):
         #     {'hint': 'Good luck!'})
         # self.response.out.write(outstr)
 
-        indeks = "file://C:/Users/Daniel/bombermanKompetencyjny/web/dist/index.html"
-        webbrowser.open(indeks, new=new)
+        # indeks = "file://C:/Users/Daniel/bombermanKompetencyjny/web/dist/index.html"
+        #webbrowser.open(indeks, new=new)
 
         # self.response.headers['Content-Type'] = 'text/plain'
         # self.response.out.write('Hello, webapp World!')
+#
+#
+# class Game(db.Model):
+#     player1 = db.StringProperty()
+#     player2 = db.StringProperty()
+#     moveUp = db.StringProperty()
+#     moveDown = db.StringProperty()
+#     moveLeft = db.StringProperty()
+#     moveRight = db.StringProperty()
+#     winner = db.StringProperty()
+#     board = db.StringProperty()
+#
+#
+# class GameUpdater():
+#     game = None
+#
+#     def __init__(self, game):
+#         self.game = game
+#
+#     def get_game_message(self):
+#         gameUpdate = {
+#             'board': self.game.board,
+#             'player1': self.game.player1,
+#             'player2': '' if not self.game.player2 else self.game.player2,
+#             'moveUp': self.game.moveUp,
+#             'moveDown': self.game.moveDown,
+#             'moveLeft': self.game.moveLeft,
+#             'moveRight': self.game.moveRight,
+#             'winner': self.game.winner,
+#         }
+#         return google.appengine._internal.django.utils.simplejson.dumps(gameUpdate)
 
+
+# def send_update(self):
+#     message = self.get_game_message()
+#     channel.send_message(self.game.player1 + self.game.key().id_or_name(), message)
+#     if self.game.userO:
+#         channel.send_message(self.game.player2 + self.game.key().id_or_name(), message)
+#
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
@@ -80,9 +107,10 @@ class LoginHandler(webapp2.RequestHandler):
         if user:
             greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
                         (user.nickname(), users.create_logout_url('/web')))
-            #przekierowywanie wszystkiego na /web a /api idzie na api
-            #zrobić to przekierowanie w pliku yaml
-        ##http://127.0.0.1:8080/_ah/login?continue=http%3A//127.0.0.1%3A8080/
+
+            # przekierowywanie wszystkiego na /web a /api idzie na api
+            # zrobić to przekierowanie w pliku yaml
+        # #http://127.0.0.1:8080/_ah/login?continue=http%3A//127.0.0.1%3A8080/
         else:
             greeting = ('<a href="%s">Sign in or register</a>.' %
                         users.create_login_url('/'))
@@ -91,6 +119,26 @@ class LoginHandler(webapp2.RequestHandler):
 
 
 application = webapp2.WSGIApplication([('/Login2', LoginHandler), ], debug=True)
+
+
+class CheckLoginStatus(base.BaseHandler):
+    """Checks the login status of a user."""
+
+    def get(self):
+        user = users.get_current_user();
+        if user:
+            response = {
+                'loggedIn': True,
+                'user': user.get_current_user(),
+                'url': return users.create_login_url('/')
+            }
+            else:
+                response = {
+                    'loggedIn': False,
+                    'url': GetLoginUrl()
+        }
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(response))
 
 
 class Memcache(webapp.RequestHandler):
@@ -118,15 +166,10 @@ class Memcache(webapp.RequestHandler):
         self.response.out.write('Proto took: %.2f' % (time.time() - t1))
 
 
-##UWAGI
-#1. Tekst(np. href) odnośnik, do przeniesienia na stronę.
-
-
 class Logout(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Bye, You have been log out')
-        #self.response.out.write(users.create_login_url("http://127.0.0.1:8080/Register"))
+        return users.create_logout_url('/logout')
 
 
 class Register(webapp.RequestHandler):
@@ -170,9 +213,9 @@ class GetEmp(webapp.RequestHandler):
         self.response.out.write(result)
 
 
-# application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 app = webapp.WSGIApplication([
                                  ('/', StronaGlowna),
+                                 ('/check_login_status', CheckLoginStatus),
                                  ('/login', LoginHandler),
                                  ('/logout', Logout),
                                  ('/register', Register),
@@ -182,6 +225,5 @@ app = webapp.WSGIApplication([
                                  ('/getemp', GetEmp),
                                  ('/add', AddEmp)
 
-                                 # poczytac jak w appengine odberac parametr z linku
                              ], debug=True)
 
